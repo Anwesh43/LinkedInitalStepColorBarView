712b8f8d43e1ad7eee9b76013443ab2f3ed4955c
Anwesh43 <anweshthecool0@gmail.com>
2020-06-04 06:43:25 +0530

    adding Int and Float extension methods

--- initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
+++ initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
@@ -19,3 +19,8 @@ val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#2196F3", "#F44336",
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
