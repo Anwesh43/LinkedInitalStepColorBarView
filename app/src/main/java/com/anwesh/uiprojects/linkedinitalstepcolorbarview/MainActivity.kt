56293e5922940e373574cda229971c8e77efa152
Anwesh43 <anweshthecool0@gmail.com>
2020-06-04 07:10:52 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedinitalstepcolorbarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedinitalstepcolorbarview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedinitalstepcolorbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.initialstepcolorbarview.InitialColorStepBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        InitialColorStepBarView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
