ff693e07f7f56545d4ff11c641fe9f89e3a6f7e7
Anwesh43 <anweshthecool0@gmail.com>
2020-06-04 06:50:18 +0530

    adding InitialColorStepBarView class

--- initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
+++ initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
@@ -48,3 +48,18 @@ fun Canvas.drawICSBNode(i : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class InitialColorStepBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
