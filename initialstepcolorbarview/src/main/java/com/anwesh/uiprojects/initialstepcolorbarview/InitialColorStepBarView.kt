c66e670bff350173ea2cc2afc850e84b81c424ed
Anwesh43 <anweshthecool0@gmail.com>
2020-06-04 06:49:00 +0530

    adding Canvas extension method

--- initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
+++ initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
@@ -24,3 +24,27 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+fun Canvas.drawColorStepBar(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = w / (bars)
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    val sf1i : Float = sf1.divideScale(i, bars)
+    val sf2i : Float = sf2.divideScale(i, bars)
+    save()
+    translate(size * i, h * sf2i)
+    drawRect(RectF(0f, 0f, size * sf1i, size), paint)
+    restore()
+}
+
+fun Canvas.drawICSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    save()
+    for (j in 0..(bars - 1)) {
+        drawColorStepBar(i, scale, w, h, paint)
+    }
+    restore()
+}
+
