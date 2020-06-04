f5da78d9ef3f1d4e143d6c0988e32fdc40855a32
Anwesh43 <anweshthecool0@gmail.com>
2020-06-04 06:42:00 +0530

    adding constants and libraries

--- initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
+++ initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
@@ -4,3 +4,18 @@ package com.anwesh.uiprojects.initialstepcolorbarview
  * Created by anweshmishra on 04/06/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val bars : Int = 5
+val scGap : Float = 0.02f / bars
+val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#2196F3", "#F44336", "#009688")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+
