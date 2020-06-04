b4da77bd5b8b6bc71f427f9a6dc573a10660ad53
Anwesh43 <anweshthecool0@gmail.com>
2020-06-04 07:00:29 +0530

    adding ICSBNode class

--- initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
+++ initialstepcolorbarview/src/main/java/com/anwesh/uiprojects/initialstepcolorbarview/InitialColorStepBarView.kt
@@ -110,4 +110,45 @@ class InitialColorStepBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ICSBNode(var i : Int, val state : State = State()) {
+
+        private var next : ICSBNode? = null
+        private var prev : ICSBNode? = null
+
+        init {
+           addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ICSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawICSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ICSBNode {
+            var curr : ICSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
